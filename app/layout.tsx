cd855e0d0b6b646e6a6cbcfc8812670d501a8c8d
Tadian Dang <118943432+MiddleHero@users.noreply.github.com>
2026-07-05 15:28:25 -0500

    Supress Hydration Warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "leaflet/dist/leaflet.css";
+
 import Navbar from "@/components/navbar";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -25,7 +25,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${inter.className} h-full antialiased`}
     >
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
