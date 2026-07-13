148d7d58e62ac71556d5e48adcf738209fd5d558
Tadian Dang <118943432+MiddleHero@users.noreply.github.com>
2026-07-12 23:57:59 -0500

    updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,8 @@ export default function About() {
       <div>Timeline</div>
       <div>Hobbies</div>
       <div>Music I Like</div>
+      <div>Spotify Widgets</div>
+      <div>Contact Redirect</div>
     </div>
   );
 }
